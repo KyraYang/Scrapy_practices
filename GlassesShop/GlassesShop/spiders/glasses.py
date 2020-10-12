d4b347252187ab1d9a8ce0c74bc49daeb938200f
Kyra <kyrayang.5@gmail.com>
2020-10-12 15:22:36 +0800

    finish practice 2

--- GlassesShop/GlassesShop/spiders/glasses.py
+++ GlassesShop/GlassesShop/spiders/glasses.py
@@ -4,8 +4,22 @@ import scrapy
 
 class GlassesSpider(scrapy.Spider):
     name = 'glasses'
-    allowed_domains = ['www.glassesshop.com/bestsellers']
+    allowed_domains = ['www.glassesshop.com']
     start_urls = ['https://www.glassesshop.com/bestsellers/']
 
     def parse(self, response):
-        pass
+        glasses_rows = response.xpath('//div[@id="product-lists"]/div')
+        for glasses in glasses_rows:
+            name = glasses.xpath(
+                './/descendant::div[@class="p-title"]/a/text()').get()
+            if not name:
+                continue
+            yield{
+                "product url": glasses.xpath('.//div[@class="product-img-outer"]/a/@href').get(),
+                "product image": glasses.xpath('.//div[@class="product-img-outer"]/a/img[1]/@src').get(),
+                "product name": name.strip(),
+                "product price": glasses.xpath('.//descendant::div[@class="p-price"]/div/span[1]/text()').get(),
+            }
+        next_page = response.xpath('//a[@rel="next"]/@href').get()
+        if next_page:
+            yield scrapy.Request(url=next_page, callback=self.parse)
