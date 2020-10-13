ca8d85dd3356e00a5b6880cb750c14b1b17e4109
Kyra <kyrayang.5@gmail.com>
2020-10-13 23:48:36 +0800

    practice 3 finished

--- BooksScrape/BooksScrape/spiders/books.py
+++ BooksScrape/BooksScrape/spiders/books.py
@@ -5,17 +5,36 @@ from scrapy.spiders import CrawlSpider, Rule
 
 
 class BooksSpider(CrawlSpider):
-    name = 'books'
-    allowed_domains = ['books.toscrape.com']
-    start_urls = ['http://books.toscrape.com/index.html/']
+    name = "books"
+    allowed_domains = ["books.toscrape.com"]
+
+    user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36"
+
+    def start_requests(self):
+        yield scrapy.Request(
+            url="http://books.toscrape.com",
+            headers={"User-Agent": self.user_agent},
+        )
 
     rules = (
-        Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+        Rule(
+            LinkExtractor(restrict_xpaths="//article"),
+            callback="parse_item",
+            follow=True,
+            process_request="set_user_agent",
+        ),
+        Rule(
+            LinkExtractor(restrict_xpaths="//li[@class='next']/a"),
+            process_request="set_user_agent",
+        ),
     )
 
+    def set_user_agent(self, request):
+        request.headers["User-Agent"] = self.user_agent
+        return request
+
     def parse_item(self, response):
-        item = {}
-        #item['domain_id'] = response.xpath('//input[@id="sid"]/@value').get()
-        #item['name'] = response.xpath('//div[@id="name"]').get()
-        #item['description'] = response.xpath('//div[@id="description"]').get()
-        return item
+        yield {
+            "title": response.xpath("//h1/text()").get(),
+            "price": response.xpath("//p[@class='price_color']/text()").get(),
+        }
